0e72defd528c9445e03d1bf40260074278ad1dc8
liuzhe <1330742453@qq.com>
2025-09-15 00:45:14 +0800

    字体，独立UI渲染

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,15 @@
 #include "spdlog/cfg/env.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include "utils.h"
+#include "ui.h"
 // 消除OpenGL弃用警告
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
 #define IMGUI_ENABLE_FREETYPE
+
+
+std::shared_ptr<spdlog::logger> logger;
+
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -19,8 +24,7 @@ int main(int, char**)
 {
     spdlog::cfg::load_env_levels("WOW_LOG_LEVEL");
     // 5MB, Max 5
-    auto wow_logger = spdlog::rotating_logger_mt("wow_logger", "wow.txt", 1048576 * 5, 5);
-    logger->info("init wow");
+    logger = spdlog::rotating_logger_mt("wow_logger", "wow.txt", 1048576 * 5, 5);
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
         return 1;
@@ -52,72 +56,29 @@ int main(int, char**)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
     // 字体
-    style.FontSizeBase = 20.0f;
-    auto font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\Deng.ttf");
-    IM_ASSERT(font != nullptr);
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    style.FontSizeBase = 40.0f;
+#ifdef _WIN32
+    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\Deng.ttf");
+#else
+    io.Fonts->AddFontFromFileTTF("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc");
+#endif
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
         if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
         {
             ImGui_ImplGlfw_Sleep(10);
             continue;
         }
-
         // 启动 dear imgui
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window) {
-            ImGui::ShowDemoWindow(&show_demo_window);
-        }
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
+        // draw
+        draw();
         // Rendering
         ImGui::Render();
         int display_w, display_h;
---
+++ src/ui.cpp
@@ -0,0 +1,37 @@
+#include "imgui.h"
+
+// Our state
+bool show_demo_window = true;
+bool show_another_window = false;
+
+
+void draw() {
+    static float f = 0.0f;
+    static int counter = 0;
+
+    ImGui::Begin("我很好");
+    ImGui::Text("你好");
+    ImGui::Checkbox("Demo Window", &show_demo_window);
+    ImGui::Checkbox("Another Window", &show_another_window);
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+    if (ImGui::Button("Button")) {
+        counter++;
+    }
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", counter);
+    ImGui::End();
+ 
+    if(show_demo_window) {
+        ImGui::ShowDemoWindow(&show_demo_window);
+    }
+
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+}
